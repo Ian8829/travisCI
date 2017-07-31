43221243f92a2679f6ff210cb5eff839471f9c90
taeshik Hwang <ghjkghj@naver.com>
2017-07-31 13:42:31 +0900

    first commit

---
+++ test/calculator_multiplication.test.js
@@ -0,0 +1,9 @@
+import { assert } from 'chai';
+
+describe('multiply()', () => {
+	it('should multiply positive numbers together');
+
+	it('should multiply positive and negative numbers together');
+
+	it('should multiply negative numbers together');
+});
---
+++ test/calculator_subtraction.test.js
@@ -0,0 +1,19 @@
+import { assert } from 'chai';
+import { subtract } from '../';
+
+describe('subtract()', () => {
+	it('should subtract positive numbers together', () => {
+		assert.equal(subtract(1, 2), -1);
+		assert.equal(subtract(3, 2), 1);
+	});
+
+	it('should subtract positive and negative numbers together', () => {
+		assert.equal(subtract(-1, 1), -2);
+		assert.equal(subtract(10, -100), 110);
+	});
+
+	it('should subtract negative numbers together', () => {
+		assert.equal(subtract(-1, -1), 0);
+		assert.equal(subtract(-4, -2), -2);
+	});
+});
